363356372bd458db36cf74b8988ce47e8d0826b3
Marcele Godoy <marcelegodoy@gmail.com>
2021-07-05 19:20:18 -0300

    Organize as admin and add info to show

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,50 +1,52 @@
-class ProductsController < ApplicationController
-  before_action :set_product, only: %i[show edit update destroy]
+module Admin
+  class ProductsController < BaseController
+    before_action :set_product, only: %i[show edit update destroy]
 
-  def index
-    @products = Product.all
-  end
-
-  def show
-  end
-
-  def new
-    @product = Product.new
-  end
-
-  def create
-    @product = Product.new(product_params)
-    if @product.save
-      redirect_to admin_product_path(@product)
-    else
-      render :new
+    def index
+      @products = Product.all
     end
-  end
 
-  def edit
-  end
-
-  def update
-    @product.update(product_params)
-    if @product.save
-      redirect_to admin_product_path(@product)
-    else
-      render :edit
+    def show
     end
-  end
 
-  def destroy
-    @product.destroy
-    redirect_to admin_products_path, notice: 'Produto deletado com sucesso'
-  end
+    def new
+      @product = Product.new
+    end
 
-  private
+    def create
+      @product = Product.new(product_params)
+      if @product.save
+        redirect_to admin_product_path(@product)
+      else
+        render :new
+      end
+    end
 
-  def set_product
-    @product = Product.find(params[:id])
-  end
+    def edit
+    end
 
-  def product_params
-    params.require(:product).permit(:name, :subtitle, :description, :price, :quantity, :active)
+    def update
+      @product.update(product_params)
+      if @product.save
+        redirect_to admin_product_path(@product)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @product.destroy
+      redirect_to admin_products_path, notice: 'Produto deletado com sucesso'
+    end
+
+    private
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :subtitle, :description, :price, :quantity, :active)
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_05_214538) do
+ActiveRecord::Schema.define(version: 2021_07_05_215702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "subtitle"
+    t.text "description"
+    t.integer "price"
+    t.integer "quantity"
+    t.boolean "active", default: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
