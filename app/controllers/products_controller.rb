9926c64b1fcffaf4dbff740c7a32f132a08db7ec
Marcele Godoy <marcelegodoy@gmail.com>
2021-07-05 21:11:32 -0300

    Add images to project

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
 
   def show
+    @q = Product.ransack(params[:q])
+    @products = @q.result
+  end
+
+  def show
     @product = Product.find(params[:id])
   end
 end
